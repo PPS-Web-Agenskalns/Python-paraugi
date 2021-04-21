ff935bfe190363f072ddfe834698df85694301f9
Linards <linardsliepenieks@gmail.com>
2021-04-21 20:19:11 +0300

    saskaitisana/atnemsana

--- atkodesanas funkcija-teika.py
+++ atkodesanas funkcija-teika.py
@@ -1,4 +1,4 @@
-ievade = "23313+123+2+12"
+ievade = "23313+123-2+12"
 #1. uzdevums
 """
 Izveidot algoritmu, kas dotajai simbolu virknei,
@@ -29,16 +29,34 @@ print(cip)
 """
 #2.uzd
 cip = 0
+summa = 0
+irNegativs = False
+
 for c in ievade:
     if ord(c)>=48 and ord(c)<=57:
        cip = cip * 10
        cip = cip + (ord(c)-48)
 
     else:
+        
+            
+        if irNegativs == True:
+            irNegativs = False
+            cip = cip * -1
+
+        if c == '-':
+            irNegativs = True
+        
+        
+
         print(cip)
+        summa = summa + cip
+        print("summa: ", summa)
         cip = 0
 
-print(cip+1000)
+print(cip)
+summa = summa + cip
+print("summa: ", summa)
 
 
 
